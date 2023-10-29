ec0a4eb63ac0d415accbf2738f7beef13c434f80
michelleduong35 <michelleduong35@gmail.com>
2023-10-28 17:08:30 -0700

    Create SOS Button
    Clickable button and shadow

--- safetywidget/page/gt/home/index.page.js
+++ safetywidget/page/gt/home/index.page.js
@@ -4,9 +4,8 @@ import { log as Logger } from "@zos/utils";
 import { TEXT_STYLE } from "zosLoader:./index.page.[pf].layout.js";
 import { createWidget, widget, prop, event } from "@zos/ui";
 const gui = new AutoGUI();
-const logger = Logger.getLogger("button1");
+/*const logger = Logger.getLogger("button1");
 
-/*
 const button1 = gui.button("Need Safety!", () => {
   my_text.update({ text: "click when safe" });
 
@@ -17,13 +16,34 @@ const button1 = gui.button("Need Safety!", () => {
 
 Page({
   build() {
-    const my_text = gui.text("click if in danger");
-    gui.newRow();
-    const button1 = gui.button("Danger", () => {
-      AutoGUI.SetColor(0x6aa84f);
-      my_text.update({ text: "click when safe" });
-      gui.render(true);
+    // Create a non-clickable filled circle beneath the clickable one
+    const shadowSOS = createWidget(widget.CIRCLE, {
+      center_x: 240,
+      center_y: 250,
+      radius: 120,
+      color: 0x3f2222,
+      alpha: 255,
     });
-    gui.render();
+
+    //create sos button
+    const buttonSOS = createWidget(widget.BUTTON, {
+      x: 120,
+      y: 105,
+      w: 240,
+      h: 240,
+      radius: 240,
+      normal_color: 0x9e2828,
+      press_color: 0x9e2828,
+      text: "SOS",
+      click_func: (button_widget) => {
+        button_widget.setProperty(prop.MORE, {
+          x: 120,
+          y: 130,
+          w: 240,
+          h: 240,
+          radius: 240,
+        });
+      },
+    });
   },
 });
